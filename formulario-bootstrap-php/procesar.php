af1a494241893f5d9d9ffb14e2a5ef95bfdafed1
Erick Reyes <erick.marin.v@gmail.com>
2018-03-06 13:33:41 -0600

    Ejercicio Whatsapp

--- formulario-bootstrap-php/procesar.php
+++ formulario-bootstrap-php/procesar.php
@@ -4,6 +4,12 @@
 	<title></title>
 </head>
 <body>
-	Procesar informacion
+	<?php 
+		var_dump($_GET);
+		echo "Nombre: " . $_GET["txt-nombre"] . "<br>";
+		echo "Gusto 0: " . $_GET["chk-gustos"][0] . "<br>";
+		echo "Pais 1: " . $_GET["slc-paises"][1] . "<br>";
+
+	?>
 </body>
 </html>
