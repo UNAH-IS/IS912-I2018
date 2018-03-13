8589199083b43ae7337b1c81357ec5237adff7c4
Erick Reyes <erick.marin.v@gmail.com>
2018-03-13 14:07:49 -0600

    Componentes

--- ejercicio-validacion-facebook/js/controlador.js
+++ ejercicio-validacion-facebook/js/controlador.js
@@ -1,15 +1,40 @@
-/*function validar(){
+function validar(){
+	validarCampoVacio("txt-nombre");
+	validarCampoVacio("txt-apellido");
+	validarCampoVacio("txt-correo");
+}
 
-}*/
 
-
-var validar = function(){
-	if (document.getElementById("txt-nombre").value == ""){
-		document.getElementById("txt-nombre").classList.remove("is-valid");
-		document.getElementById("txt-nombre").classList.add("is-invalid");
+var validarCampoVacio = function(id){
+	if (document.getElementById(id).value == ""){
+		document.getElementById(id).classList.remove("is-valid");
+		document.getElementById(id).classList.add("is-invalid");
 	}
 	else{
-		document.getElementById("txt-nombre").classList.remove("is-invalid");
-		document.getElementById("txt-nombre").classList.add("is-valid");
+		document.getElementById(id).classList.remove("is-invalid");
+		document.getElementById(id).classList.add("is-valid");
 	}
+}
+
+function validarContrasena(etiqueta){
+	if (etiqueta.value.length <6){
+		etiqueta.classList.remove("is-valid");
+		etiqueta.classList.add("is-invalid");
+	}
+	else{
+		etiqueta.classList.remove("is-invalid");
+		etiqueta.classList.add("is-valid");
+	}
+}
+
+
+function validarCorreo(etiquetaEmail) {
+    var patron = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(patron.test(String(etiquetaEmail.value).toLowerCase())){
+		etiquetaEmail.classList.remove("is-invalid");
+    	etiquetaEmail.classList.add("is-valid");
+    }else{
+    	etiquetaEmail.classList.remove("is-valid");
+    	etiquetaEmail.classList.add("is-invalid");
+    }
 }
