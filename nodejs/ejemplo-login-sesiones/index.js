dca1555cb4ea8d43af39df0835a18c5ccf486261
Erick Marín <erick.marin.v@gmail.com>
2018-05-07 20:41:58 -0600

    Ejemplo seguridad Login

--- nodejs/ejemplo-login-sesiones/index.js
+++ nodejs/ejemplo-login-sesiones/index.js
@@ -17,22 +17,59 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(express.static("public"));
 app.use(session({secret:"ASDFE$%#%",resave:true, saveUninitialized:true}));
 
+//Verificar si existe una variable de sesion para poner publica la carpeta public admin
+var publicAdmin = express.static("public-admin");
+var publicCajero = express.static("public-cajero");
+app.use(
+    function(peticion,respuesta,next){
+        if (peticion.session.correo){
+            if (peticion.session.codigoTipoUsuario == 1)
+                publicCajero(peticion,respuesta,next);
+            else if (peticion.session.codigoTipoUsuario == 2)
+                publicAdmin(peticion,respuesta,next);
+        }
+        else
+            return next();
+    }
+);
+
+///Para agregar seguridad a una ruta especifica:
+function verificarAutenticacion(peticion, respuesta, next){
+	if(peticion.session.correo)
+		return next();
+	else
+		respuesta.send("ERROR, ACCESO NO AUTORIZADO");
+}
+
 app.post("/login", function(peticion, respuesta){
     var conexion = mysql.createConnection(credenciales);
-    respuesta.send(peticion.body);
-    /*conexion.query("",function(err, data, fields){
-        peticion.session.mensaje = peticion.params.mensaje;
-        respuesta.send("Se guardo la variable de sesion");    
-    }); */   
+    conexion.query("SELECT codigo_usuario, codigo_tipo_usuario, correo, nombre, contrasena FROM tbl_usuarios WHERE correo=? and contrasena=sha1(?)",
+        [peticion.body.correo, peticion.body.contrasena],
+        function(err, data, fields){
+                if (data.length>0){
+                    peticion.session.correo = data[0].correo;
+                    peticion.session.codigoTipoUsuario = data[0].codigo_tipo_usuario;
+                    data[0].estatus = 0;
+                    respuesta.send(data[0]); 
+                }else{
+                    respuesta.send({estatus:1, mensaje: "Login fallido"}); 
+                }
+            	
+         }
+    ); 
 });
 
 app.get("/obtener-sesion", function(peticion, respuesta){
-   respuesta.send("Valor de la variable de sesion almacenado: " + peticion.session.mensaje);
+   respuesta.send("Valor de la variable de sesion almacenado: " + peticion.session.correo);
 });
 
-app.get("/destruir-sesion",function(peticion, respuesta){
+app.get("/logout",function(peticion, respuesta){
 	peticion.session.destroy();
 	respuesta.send("Sesion eliminada");
+});
+
+app.get("/ruta-restringida",verificarAutenticacion,  function(peticion, respuesta){
+    respuesta.send("Bienvenido a la ruta restringida");
 });
 
 app.listen(3000);
--- nodejs/ejemplo-login-sesiones/public/js/controlador.js
+++ nodejs/ejemplo-login-sesiones/public/js/controlador.js
@@ -1,10 +1,14 @@
 $("#btn-login").click(function(){
     $.ajax({
         url:"/login",
-        data:"usuario="+$("#usuario").val()+"&contrasena="+$("#contrasena").val(),
+        data:"correo="+$("#usuario").val()+"&contrasena="+$("#contrasena").val(),
         method:"POST",
         dataType:"json",
         success:function(respuesta){
+            if (respuesta.estatus ==0 )
+                window.location.href ="home.html";
+            else
+                alert("Credenciales incorrectas");
             console.log(respuesta);
         }
     });
